e4a39392050855df57d104207977251dce3ef2ec
Cristhian <cristhian@owqlo.com>
2025-01-24 04:48:57 +0100

    refactor(plugin-dify): reorganizar estructura de exportaciones de acciones

---
+++ packages/plugin-dify/src/actions/index.ts
@@ -0,0 +1,7 @@
+import { sendMessageAction } from "./sendMessage";
+
+// Exportamos las acciones individuales para uso directo
+export { sendMessageAction };
+
+// Exportamos todas las acciones como un namespace
+export * as actions from "./sendMessage";
